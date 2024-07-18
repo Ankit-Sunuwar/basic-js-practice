5b8a8583a87bcff6044dea07ba7df7af632b7990
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-18 17:36:05 +0545

    learned about variables

---
+++ variables.js
@@ -0,0 +1,28 @@
+// ES5
+var person = "ankit sunuwar"; // (goble variable)
+
+// ES6
+const anotherPerson = "shyam";
+let thirdPerson = "gita";
+
+console.log(person, anotherPerson, thirdPerson);
+
+var test = "sita";
+var test = "ram";
+console.log(test);
+
+const testPerson = "raktim"; // (fixed variable name declare)
+console.log(testPerson);
+
+let newPerson = "ram"; // (variable update garna dido raixa)
+newPerson = "ramayan";
+
+console.log(newPerson);
+
+// Always start with const variable.
+const table = "table";
+console.log(table); //(red table print hunxa)
+
+let phone = "23456789";
+phone = "234567";
+console.log(phone); // (234567 print hunxa)
