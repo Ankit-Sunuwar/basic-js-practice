4576d4139be633667ae37ba04a83f5dea1810062
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 07:36:18 +0545

    compled homework

---
+++ homeWork.js
@@ -0,0 +1,11 @@
+// Write a js program to create sum calculator using interaction.
+
+// alert
+alert("Welcome to calculator");
+
+// prompt
+const firstnum = parseFloat(prompt("Enter the first num:"));
+const secnum = parseFloat(prompt("Enter the second num:"));
+const sum = firstnum + secnum;
+console.log(sum);
+alert(`The sum is ${sum}`);
