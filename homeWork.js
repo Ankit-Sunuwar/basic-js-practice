96911635a44ff291560c5ea8cea0c4da7a036955
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 07:50:44 +0545

    updated homework

--- homeWork.js
+++ homeWork.js
@@ -6,6 +6,13 @@ alert("Welcome to calculator");
 // prompt
 const firstnum = parseFloat(prompt("Enter the first num:"));
 const secnum = parseFloat(prompt("Enter the second num:"));
+
+// confirm
+const sure = confirm("You want to add this two num?");
+
+// adding two number
 const sum = firstnum + secnum;
-console.log(sum);
+
+console.log(sum); // printing the sum of two number
+
 alert(`The sum is ${sum}`);
