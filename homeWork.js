338bfbfbaa865388d1d878c7d34c757a2970bb71
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 08:31:41 +0545

    finding sum of three number

--- homeWork.js
+++ homeWork.js
@@ -6,12 +6,13 @@ alert("Welcome to calculator");
 // prompt
 const firstnum = parseFloat(prompt("Enter the first num:"));
 const secnum = parseFloat(prompt("Enter the second num:"));
+const thirdnum = parseInt(prompt("Enter the third num:"));
 
 // confirm
-const sure = confirm("You want to add this two num?");
+const sure = confirm("You want to add this three num?");
 
 // adding two number
-const sum = firstnum + secnum;
+const sum = firstnum + secnum + thirdnum;
 
 console.log(sum); // printing the sum of two number
 
