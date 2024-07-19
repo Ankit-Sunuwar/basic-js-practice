c1190832b85a2fd2dc222a1f08d7da4c921ec772
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 21:42:12 +0545

    learned about statement

---
+++ statement.js
@@ -0,0 +1,47 @@
+// STatements
+// 1. if else
+// 2. switch case
+
+// 1. if and else condition
+// ask user to reply m or f type.
+
+// const gender = prompt("enter m for male and f for female");
+
+// if (gender === "m") {
+//   alert("You are male");
+// } else {
+//   alert("You are female");
+// }
+
+// = : variable assign.
+// == : data type and value compare garxa.
+// === : data type and value compare garxa + data conversion to boolean.
+
+// 2. switch case
+
+// Write a js program to check the gread of the student
+
+// const grade = Number(prompt("Enter the number:"));
+
+// switch (true) {
+//   case grade >= 80:
+//     alert("Your grade is A");
+//     break;
+//   case grade >= 60 && score < 80:
+//     alert("Your grade is B");
+//   default:
+//     alert("You have failed.");
+// }
+
+//ES5
+const gender = "m";
+// // if (gender === "m");{
+// console.log("m");
+// } else{
+//     console.log(f);
+// }
+
+//ES6
+// Syntax
+// condition ? true : false;
+gender === "m" ? console.log("m") : console.log("f"); //"m"
