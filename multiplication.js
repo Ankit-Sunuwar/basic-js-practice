ff1ec08d388bbbd3eb7ea85bd475c4138a11af3c
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 12:26:42 +0545

    multipiled the numbers

---
+++ multiplication.js
@@ -0,0 +1,18 @@
+// Write a js program to create multipication calculator using interaction.
+
+// alert
+alert("Welcome to calculator");
+
+// prompt
+const firstnum = parseFloat(prompt("Enter the first num:"));
+const secondnum = parseFloat(prompt("Enter the second num:"));
+
+// confirm
+const sure = confirm("You want to add this two num?");
+
+// mutiplying two number
+const multiplication = firstnum * secondnum;
+
+console.log(multiplication); // printing the sum of two number
+
+alert(`The multiplication is ${multiplication}`); //string literal
