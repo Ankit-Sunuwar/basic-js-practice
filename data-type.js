d91ab9fdd1b229d69edfb045c654ad93d346908b
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-19 17:06:42 +0545

    learned about data-type

---
+++ data-type.js
@@ -0,0 +1,8 @@
+// Data type (5 types)
+// 1. Primitive Datatype
+// 2. Complex Datatype
+// type conversion / coercion
+// const a = "2";
+// consol.log(typeof a);  // check type
+// const num = Number(a); // type conversion
+// consol.log(num)
