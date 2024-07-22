3cf13a70ff46cb9c72e307ea3f17939f81c1c19d
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-22 16:39:13 +0545

    converted fahrenheit to celsius

---
+++ temperature-2.js
@@ -0,0 +1,10 @@
+// Fahrenheit temperature
+const fahrenheitTemperature = 45;
+
+// Conversion formula: (f-32) × 5/9
+const celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+
+// Output the result
+console.log(
+  `${fahrenheitTemperature} degrees fahrenheit is equal to ${celsiusTemperature} degrees celsius.`
+);
