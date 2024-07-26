864afaa436ac8e191e8f2adaf15a8df58ab50be3
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-07-26 17:11:11 +0545

    learned about arrays

---
+++ arrays.js
@@ -0,0 +1,61 @@
+// array ( complex data type )
+// collection of items
+// CRUD
+
+// Create
+
+// const arr = new Array();
+// console.log({ arr });
+
+// const arrV1 = [];
+
+// //create
+// const arr = new Array();
+// console.log ({ arr });
+// const arrV1 = [];
+// const cars = [
+//   { name: "Tesla", model: 2021 },
+//   { name: "TATA", model: 2020 },
+//   {
+//     name: "Pourse",
+//     model: 2017,
+//   },
+//   {
+//     name: "Scorpio",
+//     model: 2024,
+//   },
+// ];
+// // Read
+// console.log(cars[0]);
+// // Update
+// //const copiesOfCars = cars; // referential value X (yes le dui ota value update gar xa. X
+// const copyOfCars = [...cars];
+// const newName = "Tesla Model X";
+// carsOfcars[0].name = newName;
+// console.log({ copyOfCars, cars });
+// // delete
+// // delete copyOfCars[0]
+// const [a, ...rest] = copyOfCars;
+// console.log({ rest });
+
+// Array operations (methods)
+
+// map, filter, reduce, slice, some, every, includes, find, sort
+
+// Write a js function that sorts the age in ascendung order
+
+// const peoples = [
+//   { name: "raktim", age: 30 },
+//   { name: "anjali", age: 18 },
+//   { name: "paras", age: 25 },
+// ];
+
+// const ageSorter = {arr} => {
+// };
+// const result = ageSorter
+// Output
+// [
+//   { name: "anjali", age: 18  }
+//    { name: "paras", age: 25 }
+//    { name: "raktim", age: 30  }
+// ];
